bb235b3996a5494c64fdbe3b4b7681b3d7b0bbd5
elshabrawy <mohamed.saeed.elshabrawy@gmail.com>
2021-12-31 14:27:20 +0200

    add comment

--- src/main/java/com/accenture/projecttask/ProjectTaskApplication.java
+++ src/main/java/com/accenture/projecttask/ProjectTaskApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectTaskApplication {
 
 	public static void main(String[] args) {
+		//comment
 		SpringApplication.run(ProjectTaskApplication.class, args);
 	}
 
