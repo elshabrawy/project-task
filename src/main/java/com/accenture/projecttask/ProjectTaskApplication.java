f9010b03f55d2aef8f032ecda93f1c642d58dfde
elshabrawy <mohamed.saeed.elshabrawy@gmail.com>
2021-12-31 14:32:33 +0200

    Revert "222222"
    This reverts commit 7e9860697fb5e5d86baa5df1d618f88d6723426b.

--- src/main/java/com/accenture/projecttask/ProjectTaskApplication.java
+++ src/main/java/com/accenture/projecttask/ProjectTaskApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectTaskApplication {
 
 	public static void main(String[] args) {
-		
-		// test
+		//comment
 		SpringApplication.run(ProjectTaskApplication.class, args);
 	}
 
